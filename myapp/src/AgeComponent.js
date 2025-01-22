49d1875796e5655b969eb62a114730364e57dfa1
LaviRastogi <1993lavirastogi@gmail.com>
2025-01-22 19:02:03 +0530

    react state

--- myapp/src/AgeComponent.js
+++ myapp/src/AgeComponent.js
@@ -1,7 +1,18 @@
-function AgeComponent({ user }) {
+import { use, useState } from "react";
+import { useEffect } from "react";
+
+function AgeComponent(props) {
+  console.log(props.age);
+  //iniatal - 34
+  const [age1, setAge1] = useState(Number(props.age));
+
+  useEffect(() => {
+    setAge1(Number(props.age));
+  }, [props.age]);
+
   return (
     <div>
-      <p>Age: {user.age}</p>
+      <p>My age is {age1}</p>
     </div>
   );
 }
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,27 +1,35 @@
+import React, { useState } from "react";
 import AgeComponent from "./AgeComponent";
-import HeadingComponent from "./HeadingComponent";
-import HeroImg from "./images/images1.jpg";
 
-function App() {
-  let user = {
-    name: "Lavi",
-    age: 24,
-    city: "Delhi",
+export default function App() {
+  let [age, setAge] = useState(30);
+  const [obj, setObj] = useState({ name: "lavi", age: 30 });
+  const [arr, setArr] = useState([1, 2, 3, 4]);
+
+  const clickHandler = () => {
+    // setAge(age + 1);
+    // setObj((obj.name = "labi"));
+    // setObj({ ...obj, age: age + 1, name: "lllll" });
+    // let newArr = [1, ...arr];
+    // setArr(arr.push(1, 2, 6));
+    // arr.push(1, 2, 6);
+    // setAge(30);
+    // setAge(age + 1);
+    // setAge(age + 1);
+    // setAge(age + 1);
+    // setAge(age + 2);
+    // setAge((prev) => prev + 1);
+    // setAge((prev) => prev + 1);
+    // setAge((prev) => prev + 1);
+    setAge((prev) => prev + 1);
+    setArr(arr.push(1, 2, 6));
+    setObj({ ...obj, age: age + 1, name: "lllll" });
   };
   return (
     <div>
-      <img src={HeroImg} alt="HeroImg" width={100} height={100} />
-      <h1>
-        Welcome {user.name} and he is {user?.age > 22 ? "adult" : "child"}
-      </h1>
-      {/* props example pass object,data,function anything as a prop */}
-      <HeadingComponent heading="Our first React component" />
-      <HeadingComponent heading="Our first React component" />
-      <HeadingComponent heading="Our first React component" />
-      <HeadingComponent heading="Our first React component" />
-      <AgeComponent user={user} />
+      {/* <AgeComponent age={age} /> */}
+      <h1>{age}</h1>
+      <button onClick={clickHandler}> Click</button>
     </div>
   );
 }
-
-export default App;
